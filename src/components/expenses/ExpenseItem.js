6486b23d61aa3328b04da8c2b163eb5af40dbe67
Paulo García <psebastian21@gmail.com>
2023-05-16 17:05:57 -0500

    Added state

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../misc/containers/Card"
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title)
+  const onClickHandler = () => {
+    setTitle("Updated!")
+    console.log(title)
+  }
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={onClickHandler}>Change title</button>
     </Card>
   );
 }
