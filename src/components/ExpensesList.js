9f93b3e9aeb42dbdc768f4c5270ed67d9b64b9bf
Paulo García <psebastian21@gmail.com>
2023-05-15 21:22:25 -0500

    Added Card wrapper

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,9 +1,10 @@
 import './ExpensesList.css'
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
 
 function ExpensesList(props) {
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
@@ -24,7 +25,7 @@ function ExpensesList(props) {
                 amount={props.expenses[3].amount}
                 date={props.expenses[3].date}
             />
-        </div>
+        </Card>
     )
 }
 
