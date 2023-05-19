56b89ee5acc5d1f1a7236b0f24165e8d5e5c23f5
Paulo García <psebastian21@gmail.com>
2023-05-19 00:18:23 -0500

    multi state

--- src/components/expenses/newexpense/ExpenseForm.js
+++ src/components/expenses/newexpense/ExpenseForm.js
@@ -1,21 +1,39 @@
 import './ExpenseForm.css'
 
+import { useState } from 'react'
+
 const ExpenseForm = () => {
     const today = getToday()
+
+    const [title, setTitle] = useState('')
+    const titleChangeHandler = (event) => {
+        setTitle(event.target.value)
+    }    
+
+    const [amount, setAmount] = useState('')
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value)
+    }
+
+    const [date, setDate] = useState('')
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value)
+    }
+
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2000-01-01' max={today} />
+                    <input type='date' min='2000-01-01' max={today} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <button type='submit'>Add Expense</button>
